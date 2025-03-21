b60f1d0920f3b075aab8b4d82981eea38337f770
anuragkumar-git <patelanurag3971@gmail.com>
2025-03-21 22:18:07 +0530

    chore: initialize backend server with Express, configure environment variables, and set up base routes

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`🖥️  Server running at\x1b[34m http://localhost:${port}\x1b[0m`);
+})
