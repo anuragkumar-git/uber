3cf9f3883ea022d37d6b04322cb9abdb818c6e07
anuragkumar-git <patelanurag3971@gmail.com>
2025-03-23 16:10:34 +0530

    docs: add comments for better code readability

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,23 @@ dotenv.config();
 const express = require('express');
 const app = express();
 
+const cors = require('cors')
+const dbConnection = require('./db/connection')
+const userRoutes = require('./routes/userRoutes')
+
+dbConnection()
+
+// Middleware for cross-origin requests and JSON parsing
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Base route
 app.get('/', (req, res) => {
     res.send(`Server is ready`);
 });
 
+// User routes
+app.use('/users', userRoutes)
+ 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+// Create HTTP server
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`🖥️  Server running at\x1b[34m http://localhost:${port}\x1b[0m`);
